ac66471ec7e4aadd4b29ac1a9e1a751770efa4c8
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-04-10 14:29:46 +0100

    Tryed with absolute path

--- WebApp/pages/AI.py
+++ WebApp/pages/AI.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from pathlib import Path
 
-__path__ = Path("../static/chatGPT_AI/processedData.csv")
+
+import os
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+__path__ = os.path.join(APP_ROOT, "static/chatGPT_AI/processedData.csv")
+
+#__path__ = Path("static/chatGPT_AI/processedData.csv")
 st.write("A statistical analysis of the sentiments manifested about AI and ChatGPT")
 
 with open(__path__, "r") as fCSV:
